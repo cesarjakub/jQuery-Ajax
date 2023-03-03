ffc319b80f10e769b39e898e72cb5e849d26ce85
cesarjakub <list>
2023-03-03 20:36:35 +0100

    feat: coin table + errors

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 let showError = document.getElementById("Error");
 showError.style.visibility = "hidden";
 let searchText = document.getElementById("searchInput");
-let searchBtn = document.getElementById("searchBtn");
+
+function cards(img, title, czk, usd, eur){
+    let text = `
+    <tbody class="table-group-divider">
+      <tr>
+        <img src="${img}" alt="img">
+        <td>${title}</td>
+        <td>${czk}kč</td>
+        <td>${usd}$</td>
+        <td>${eur}€</td>
+      </tr>
+    </tbody>`;
+    return text;
+}
 
 function getCoinID(id){
     $.ajax({
@@ -12,7 +25,10 @@ function getCoinID(id){
         },
         success: function(result){
             console.log(result);
-            console.log("eur = "+result.market_data.current_price.eur);
+            let czk = result.market_data.current_price.czk;
+            let eur = result.market_data.current_price.eur;
+            let usd = result.market_data.current_price.usd;
+            $(".table").append(cards(result.image.large ,result.name, czk, eur, usd));
             showError.style.visibility = "hidden";
         },
         error: function(error){
@@ -21,15 +37,10 @@ function getCoinID(id){
     });
 }
 
-function getCoinCurrency(){
-    $.ajax({
-
-    });
-}
-
 $("#searchBtn").click(function(e){
     getCoinID(searchText.value);
 })
+
 
 
 
