c190533c877f998b752570db61617683759105fd
cesarjakub <list>
2023-03-06 16:42:07 +0100

    feat: data table values

--- script.js
+++ script.js
@@ -7,17 +7,13 @@ $( document ).ready(() => {
 
     function cards(img, title, czk, usd, eur){
         let text = `
-                    <div class="card" style="width: 18rem;">
-            <img src="${img}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${title}</h5>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">CZK: ${czk}</li>
-                <li class="list-group-item">EUR: ${eur}</li>
-                <li class="list-group-item">USD: ${usd}</li>
-            </ul>
-            </div>`;
+        <tr>
+        <th scope="row"><img src="${img}" alt="coin"></th>
+        <td>${title}</td>
+        <td>${czk}kč</td>
+        <td>${usd}$</td>
+        <td>${eur}€</td>
+        </tr>`;
         return text;
     }
 
@@ -33,31 +29,14 @@ $( document ).ready(() => {
                 let czk = result.market_data.current_price.czk;
                 let eur = result.market_data.current_price.eur;
                 let usd = result.market_data.current_price.usd;
-                $(".cards").append(cards(result.image.small, result.name, czk, eur, usd));
+                $("tbody").prepend(cards(result.image.thumb, result.name, czk, eur, usd));
                 $("#Eroor").css("visibility","hidden");
+                $(".table").css("visibility","visible");
             },
             error: function(error){
                 $("#Error").css("visibility","visible");
                 
             } 
-        });
-    }
-
-    function coinList(){
-        $.ajax({
-            url: ``,
-            type: "GET",
-            data:{
-
-            },
-            success: function(result){
-                
-                $("#Eroor").css("visibility","hidden");
-            },
-            error: function(error){
-                $("#Error").css("visibility","visible");
-                
-            }
         });
     }
 
